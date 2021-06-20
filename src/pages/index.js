86bb362fe2d6e649aaf8202cd144bdfddf6aa92a
Thiago Fernando <thyagogoth@gmail.com>
2021-06-19 21:48:08 -0300

    Material-UI Configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from 'react'
+// import React from 'react'
+
+import { Button } from '@material-ui/core'
 
 export default function Home() {
-	return <div>Clone youtube</div>
+	return (
+		<>
+			Clone Youtube
+			<Button variant="outlined" color="primary">
+				PRIMARY
+			</Button>
+		</>
+	)
 }
